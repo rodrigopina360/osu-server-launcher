ebcb0c14b032db5b32d75dc1000d457aa15c41de
Rodrigo Pina <rodrigooliveirapina@gmail.com>
2024-03-13 02:05:41 +0000

    Update game location detection to osu! installs outside "osu!" folder

--- osu! Server Launcher/Utils/OsuUtil.cs
+++ osu! Server Launcher/Utils/OsuUtil.cs
@@ -9,46 +9,49 @@ using osuserverlauncher.ViewModels;
 
 namespace osuserverlauncher.Utils
 {
-  public class OsuUtil
-  {
+    public class OsuUtil
+    {
     public static void SetServerCredentials(string configFile, ServerViewModel server)
     {
-      string[] lines = File.ReadAllLines(configFile);
-      for (int i = 0; i < lines.Length; i++)
+        string[] lines = File.ReadAllLines(configFile);
+        for (int i = 0; i < lines.Length; i++)
         if (lines[i].StartsWith("Username ="))
-          lines[i] = $"Username = {server.Credentials.Username}";
+            lines[i] = $"Username = {server.Credentials.Username}";
         else if (lines[i].StartsWith("Password ="))
-          lines[i] = $"Password = {server.Credentials.PlainPassword}";
+            lines[i] = $"Password = {server.Credentials.PlainPassword}";
         else if (lines[i].StartsWith("SaveUsername ="))
-          lines[i] = $"SaveUsername = 1";
+            lines[i] = $"SaveUsername = 1";
         else if (lines[i].StartsWith("SavePassword ="))
-          lines[i] = $"SavePassword = 1";
+            lines[i] = $"SavePassword = 1";
         else if (lines[i].StartsWith("CredentialEndpoint ="))
-          lines[i] = $"CredentialEndpoint = {(server.IsBancho ? "" : server.Domain)}";
+            lines[i] = $"CredentialEndpoint = {(server.IsBancho ? "" : server.Domain)}";
 
-      File.WriteAllLines(configFile, lines);
+        File.WriteAllLines(configFile, lines);
     }
 
     public static string GetOsuPath()
     {
-      if (File.Exists(Path.Combine(Environment.GetEnvironmentVariable("localappdata"), "osu!", "osu!.exe")))
-        return Path.Combine(Environment.GetEnvironmentVariable("localappdata"), "osu!");
+            if (File.Exists(Path.Combine(Environment.GetEnvironmentVariable("localappdata"), "osu!", "osu!.exe")))
+            {
+                return Path.Combine(Environment.GetEnvironmentVariable("localappdata"), "osu!");
+            }
 
-      try
-      {
-        // "F:\\osu!\\osu.exe",1
-        string osuexe = Registry.GetValue("HKEY_CLASSES_ROOT\\osu\\DefaultIcon", "", "").ToString().Split('"')[1];
-        string osudir = new FileInfo(osuexe).DirectoryName;
+            try
+            {
+                string osuexe = Registry.GetValue("HKEY_CLASSES_ROOT\\osu\\DefaultIcon", "", "").ToString().Split('"')[1];
+                string osudir = new FileInfo(osuexe).DirectoryName;
 
-        // exclude possible custom clients (folder or exe not named "osu!")
-        if (new DirectoryInfo(osudir).Name != "osu!" || !File.Exists(Path.Combine(osudir, "osu!.exe")))
-          return null;
+                if (!File.Exists(Path.Combine(osudir, "osu!.exe")))
+                {
+                    return string.Empty;
+                }
 
-        return osudir;
-      }
-      catch { }
-
-      return null;
+                return osudir;
+            }
+            catch 
+            {
+                return null;
+            }
+        }
     }
-  }
 }
